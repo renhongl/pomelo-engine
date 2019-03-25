3fdff141936b62478188bc13e9e8544e5c07a96a
Liang RenHong <1075220132@qq.com>
2019-03-25 23:00:40 +0800

    Update
    Update

---
+++ src/pomelo-engine/index.js
@@ -0,0 +1,29 @@
+import {
+  RenderObj,
+  Scene,
+  SceneManager,
+  EventListener,
+  FrameState,
+  Game,
+  utils,
+  Sprite,
+  Frames,
+  Animations,
+  FrameCtrl,
+  Key
+} from "./core";
+
+export {
+  RenderObj,
+  Scene,
+  SceneManager,
+  EventListener,
+  FrameState,
+  Game,
+  utils,
+  Sprite,
+  Frames,
+  Animations,
+  FrameCtrl,
+  Key
+};
