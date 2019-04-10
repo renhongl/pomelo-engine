50a811fe8e4c3a2a31ec6c886d3fa2f66a6f5daa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-10 06:14:07 +0000

    Deploy renhongl/pomelo-engine to github.com/renhongl/pomelo-engine.git:gh-pages

---
+++ precache-manifest.4fbdba7dcd9facad4ed05c84cc57036d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ef5c5b905f80dc4b7e8",
+    "url": "/pomelo-engine/static/css/main.3a5c2b43.chunk.css"
+  },
+  {
+    "revision": "2ef5c5b905f80dc4b7e8",
+    "url": "/pomelo-engine/static/js/main.26c471b8.chunk.js"
+  },
+  {
+    "revision": "d4b0dad3ecc65176c699",
+    "url": "/pomelo-engine/static/js/runtime~main.e662d6db.js"
+  },
+  {
+    "revision": "8fc05fbc3b3e2cea89d7",
+    "url": "/pomelo-engine/static/css/2.b8ddabc7.chunk.css"
+  },
+  {
+    "revision": "8fc05fbc3b3e2cea89d7",
+    "url": "/pomelo-engine/static/js/2.2c1d6003.chunk.js"
+  },
+  {
+    "revision": "7992515846e228313f62",
+    "url": "/pomelo-engine/static/js/3.5724901c.chunk.js"
+  },
+  {
+    "revision": "e05dfc29559eb8f6de27",
+    "url": "/pomelo-engine/static/js/4.60bf961f.chunk.js"
+  },
+  {
+    "revision": "ee9021bb1427d23fd860",
+    "url": "/pomelo-engine/static/js/5.0532635d.chunk.js"
+  },
+  {
+    "revision": "8bc940c91a0090b53af2",
+    "url": "/pomelo-engine/static/js/6.31a363b0.chunk.js"
+  },
+  {
+    "revision": "431df6584596cb7780aa",
+    "url": "/pomelo-engine/static/js/7.88bdecc8.chunk.js"
+  },
+  {
+    "revision": "c71f91b8c56a5844e214",
+    "url": "/pomelo-engine/static/js/8.fb6274bf.chunk.js"
+  },
+  {
+    "revision": "6b0e592b4b27a8d2d571",
+    "url": "/pomelo-engine/static/js/9.b7d9e299.chunk.js"
+  },
+  {
+    "revision": "a16d9092a6207d1f3ecf",
+    "url": "/pomelo-engine/static/js/10.72d73450.chunk.js"
+  },
+  {
+    "revision": "306e92b5c34246a47ea3440eaa4a6ffe",
+    "url": "/pomelo-engine/index.html"
+  }
+];
