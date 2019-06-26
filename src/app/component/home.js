de8395d7e92aa495a0b007a41494f084450c4069
Liang RenHong <liangrenhong2017@gmail.com>
2019-06-26 15:58:06 +0800

    update
    update

--- src/app/component/home.js
+++ src/app/component/home.js
@@ -1,8 +1,70 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import Icon from "@material-ui/core/Icon";
+import "pomelo-engine/styles.css";
+import { Game } from "pomelo-engine/core";
+import { RenderObj, utils } from "pomelo-engine/core";
+
+export class Ball extends RenderObj {
+  constructor(args) {
+    super(args);
+    this.r = args.r || 10;
+    this.color = args.color || "#000";
+  }
+
+  update() {
+    let w = this.owner.w,
+      h = this.owner.h;
+
+    if (this.x < this.r || this.x > w - this.r) {
+      this.dx = -this.dx;
+    }
+    if (this.y < this.r || this.y > h - this.r) {
+      this.dy = -this.dy;
+    }
+    super.update();
+  }
+
+  render(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.r - 3, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.strokeStyle = "#000";
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+    ctx.stroke();
+  }
+}
 
 export default class Home extends React.Component {
+  componentDidMount() {
+    let container = document.querySelector(".normal-content");
+    let w = container.clientWidth;
+    let h = container.clientHeight;
+    let game = new Game({ container });
+    let scene = game.sceneManager.createScene({
+      name: "background",
+      x: 30,
+      y: 10,
+      w,
+      h
+    });
+    game.run(60);
+    for (let i = 0; i < 50; i++) {
+      let ball = new Ball({
+        x: Math.random() * 500 + 20,
+        y: Math.random() * 400 + 20,
+        r: 10,
+        dx: Math.random() * 3 + 1,
+        dy: Math.random() * 3 + 1,
+        color: utils.randomColor()
+      });
+      scene.addRObj(ball);
+    }
+  }
+
   render() {
     const { changePage, changeDocNumber } = this.props;
     return (
---
+++ src/example/exa-planeGame.js
@@ -0,0 +1,5 @@
+import BaseExample from "./baseExample";
+
+export default class Example extends BaseExample {
+  render() {}
+}
